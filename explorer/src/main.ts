94da44d2a8e9d88705ee1f39df6029694785f803
nath.selendra <nath.selendra@gmail.com>
2023-09-13 08:45:29 +0700

    update

--- explorer/src/main.ts
+++ explorer/src/main.ts
@@ -1,8 +1,7 @@
-import { Processor, processBlock, processBalances, processAccounts } from './process'
+import { Processor, processBlock, processAccounts } from './process'
 import { TypeormDatabase } from '@subsquid/typeorm-store'
 
 Processor.run(new TypeormDatabase(), async (ctx) => {
   // await processBlock(ctx)
-  // await processBalances(ctx)
   await processAccounts(ctx)
 })
